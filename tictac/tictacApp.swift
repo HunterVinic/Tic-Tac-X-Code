59630e600d07fb70f112b946cfc9b243a7a0b8b7
HunterVinic <sheshehangs2016@gmail.com>
2023-01-18 15:23:23 +0700

    Initial Commit

---
+++ tictac/tictacApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictacApp.swift
+//  tictac
+//
+//  Created by Sese on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct tictacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
